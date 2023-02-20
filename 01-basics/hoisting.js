f9f70935db32252ccd4dddbf7da75a81d7207d5c
Apodlubny <apodlubny@gmail.com>
2023-02-20 20:24:45 +0200

    Update hoisting.js

--- 01-basics/hoisting.js
+++ 01-basics/hoisting.js
@@ -6,3 +6,14 @@ function makeMessage() {
   return "works";
 }
 // такий код спрацює
+
+// Приклад function expression
+
+makeMessage();
+const makeMessage = () => {
+  return "works";
+};
+// такий код не спрацює оскільки в f expression hoisting не працює
+
+//es5
+/* Змінна обявлена без var буде обявлена в глобальному скоупі */
