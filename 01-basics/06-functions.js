1f3179130565827b6df72c7b662375f34a73b885
Apodlubny <apodlubny@gmail.com>
2023-02-23 20:23:03 +0200

    funcs

---
+++ 01-basics/06-functions.js
@@ -0,0 +1,15 @@
+/* 
+види функцій
+ */
+//function declaration
+
+function someFunc() {}
+
+//function expression
+const someAnotherFunc = function () {};
+
+//arrow function
+const arrowFunc = (a, b) => a * b;
+
+//IIFE immediately invoked function expression
+(function () {})();
